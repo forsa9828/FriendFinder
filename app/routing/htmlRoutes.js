f36e458c6718769ad6e584125ee3a06ca40892c0
forsa9828 <57837749+forsa9828@users.noreply.github.com>
2020-03-11 16:23:23 -0500

    Correcting code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,30 +1,21 @@
-// A GET Route to `/survey` which should display the survey page.
+
+var path = require('path');
+
+function htmlRoutes(app) {
+  
+// A GET Route to `/survey` which should display the survey page.  
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, +"/../public/survey.html"));
+});
+  
 //A default, catch-all route that leads to `home.html` which displays the home page.
+app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
 
-app.get("/home", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
 
-// Display survey
-app.get("/api/survey", function(req, res) {
-  return res.json(survey);
-});
+}
 
-// Displays survey, or returns false
-app.get("/api/survey/:survey", function(req, res) {
-  var chosen = req.params.survey;
-
-  console.log(chosen);
-
-  for (var i = 0; i < survey.length; i++) {
-    if (chosen === survey[i].routeName) {
-      return res.json(survey[i]);
-    }
-  }
-
-  return res.json(false);
-});
+//Export for use in server.js file
+module.exports = htmlRoutes;
